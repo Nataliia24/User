2fea21066cadfbe98957d648e039a9612e3e0c52
Nataliia <opruskon24@gmail.com>
2020-03-24 18:18:08 +0200

    create-user

--- src/app/modules/address/data.service.spec.ts
+++ src/app/modules/address/data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AddressService } from './address.service';
+import { DataService } from './data.service';
 
-describe('AddressService', () => {
-  let service: AddressService;
+describe('DataService', () => {
+  let service: DataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AddressService);
+    service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
